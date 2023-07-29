eb6f0bdafbb4698efb4b65d17a8dfade012fee18
Hydroper <110610266+hydroper@users.noreply.github.com>
2023-07-29 17:25:04 -0300

    Update lib.rs

--- c3/src/lib.rs
+++ c3/src/lib.rs
@@ -1,8 +1,8 @@
 pub fn f() {
     #[cfg(feature = "c1_some_feature")] {
-        println!("called with c1_some_feature");
+        println!("called c3::f() with c1_some_feature");
     }
     #[cfg(not(feature = "c1_some_feature"))] {
-        println!("called without c1_some_feature");
+        println!("called c3::f() without c1_some_feature");
     }
 }
